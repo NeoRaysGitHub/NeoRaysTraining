5eca3ef0e29783a79ca7a97d9085f912282d3e51
pradeep.kp@clusterindia.com <pradeep.kp@clusterindia.com>
2018-04-07 13:22:14 +0530

    initial commit

---
+++ MS_01_LOGIN/src/main/java/com/neorays/dto/EmployeeDTO.java
@@ -0,0 +1,59 @@
+package com.neorays.dto;
+
+import java.io.Serializable;
+
+import javax.annotation.Generated;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "Employee")
+public class EmployeeDTO implements Serializable {
+	@Id
+	@GenericGenerator(name = "emp", strategy = "increment")
+	@GeneratedValue(generator = "emp")
+	@Column(name = "e_id")
+	private int eid;
+	@Column(name = "e_name")
+	private String eName;
+	@Column(name = "e_sal")
+	private double esal;
+
+	/* Default constructors */
+	
+	public EmployeeDTO() {
+		System.out.println(this.getClass().getCanonicalName());
+	}
+
+	/* SETTERS AND GETTERS */
+
+	public int getEid() {
+		return eid;
+	}
+
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+
+	public String geteName() {
+		return eName;
+	}
+
+	public void seteName(String eName) {
+		this.eName = eName;
+	}
+
+	public double getEsal() {
+		return esal;
+	}
+
+	public void setEsal(double esal) {
+		this.esal = esal;
+	}
+
+}
