890769677bc2b9fa9aa1717f08e956b0c539dbf5
Neorays-PC\Neorays <kaveri.hi@clusterindia.com>
2018-04-09 09:29:33 +0530

    struts internationalization program added

---
+++ strutsexamples/dg/src/com/cluster/form/LoginForm.java
@@ -0,0 +1,29 @@
+package com.cluster.form;
+
+
+import org.apache.struts.action.ActionForm;
+public class LoginForm extends ActionForm
+{
+ private String userName;
+ private String password;
+ 
+ public void setUserName(String userName)
+ {
+	 this.userName=userName;
+ }
+ 
+ public String getUserName()
+ {
+	 return userName;
+ }
+ 
+ public void setPassword(String password)
+ {
+	 this.password=password;
+ }
+ 
+ public String getPassword()
+ {
+	 return password;
+ }
+}
