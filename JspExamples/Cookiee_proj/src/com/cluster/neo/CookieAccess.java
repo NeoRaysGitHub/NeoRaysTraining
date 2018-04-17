f07eb1eb8db2f4ee225ea47d5afb703c5389e32a
NeoRays-PC\NeoRays <deepashree.m@clusterindia.com>
2018-04-17 17:37:04 +0530

    cookie

---
+++ JspExamples/Cookiee_proj/src/com/cluster/neo/CookieAccess.java
@@ -0,0 +1,42 @@
+package com.cluster.neo;
+
+import java.io.IOException;
+
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/login")
+public class CookieAccess extends HttpServlet
+{
+	public void doGet(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
+		res.setContentType("text/html");
+		PrintWriter pw = res.getWriter();
+		String strFname = req.getParameter("fname");
+		String strLname = req.getParameter("lname");
+		
+		Cookie c1 = new Cookie("FNAME",strFname);
+		Cookie c2 = new Cookie("LNAME",strLname);
+		
+		res.addCookie(c1);
+		res.addCookie(c2);
+		
+		pw.println("<html>");
+		pw.println("<body bgcolor='yellow'>");
+		pw.println("<form action='./jsp/sign.jsp'");
+		pw.println("<center><h2>PROFFESIONAL DETAILS</center>");
+		pw.println("<table>");
+		pw.println("<tr><td>Enter your current company name</td>");
+		pw.println("<td><input type='text' name='compname'></td></tr>");
+		pw.println("<tr><td>Enter your current salary</td>");
+		pw.println("<td><input type='text' name='sal'></td></tr>");
+		pw.println("<tr><td><input type='submit' value='Submit'></td></tr>");
+		pw.println("</form></body></html>");
+	
+	}
+}
