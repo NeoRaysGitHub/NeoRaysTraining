4640818f1f196b9e1219afe3af4c7fa2a17fd163
NeoRays-PC\NeoRays <bharathkumar.c@clusterindia.com>
2018-04-18 17:59:27 +0530

    employee project

---
+++ jspExamples/EMPLOYEEPROJECT/WebContent/registration.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body bagcolour="pink">
+<form action ="register.bharath" method="post"></form>
+<h4>FIRST NAME:<input type="text" name='firstname'></h4>
+</br>
+<h4>LAST NAME:<input type="txt" name='lastname'></h4>
+<input type="submit" value="SUBMIT">
+</form>
+
+</body>
+</html>
---
+++ jspExamples/EMPLOYEEPROJECT/src/com/bharath/servlets/registrationServlet.java
@@ -0,0 +1,44 @@
+package com.bharath.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class registrationServlet
+ */
+@WebServlet("/registrationServlet")
+public class registrationServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public registrationServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	
+String firstname=request.getParameter("fistname");
+String lastname=request.getParameter("lastname");
+System.out.println(firstname);
+System.out.println(lastname);
+	}
+
+}
