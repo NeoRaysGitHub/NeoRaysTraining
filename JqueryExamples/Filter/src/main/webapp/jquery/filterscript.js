6c239311996c55e5a2871e35594d0bdce07f5458
NeoRays-PC\NeoRays <deepashree.m@clusterindia.com>
2018-05-03 18:26:47 +0530

    adding filters

---
+++ JqueryExamples/Filter/src/main/webapp/jquery/filterscript.js
@@ -0,0 +1,30 @@
+// used to filter the contents in table
+$(document).ready(function filtertable(){
+	  $("#myInput").on("keyup", function() {
+	    var value = $(this).val().toLowerCase();
+	    $("#myTable tr").filter(function() {
+	      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+	    });
+	  });
+	});
+	
+//used to filter the contents in the list	
+$(document).ready(function filterlist(){
+  $("#myInput1").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myList li").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
+
+
+//used to filter the contents in div tag
+$(document).ready(function filterdiv(){
+	  $("#myInput2").on("keyup", function() {
+	    var value = $(this).val().toLowerCase();
+	    $("#myDIV *").filter(function() {
+	      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+	    });
+	  });
+	});
