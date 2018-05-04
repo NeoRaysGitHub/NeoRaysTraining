3a1511a36d7e9e9b48cbdd925927c0a5a6b94db1
Neorays-PC\Neorays <kaveri.hi@clusterindia.com>
2018-05-04 12:56:39 +0530

    weather app in reactjs

---
+++ reactJS_Examples/weather_app/src/components/Titles.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Titles extends React.Component{
+  render(){
+    return(
+      <div>
+      <h1>  weather finder </h1>
+      <p> Find out weather temperature, conditions and more...</p>
+      </div>
+    )
+  }
+}
+export default Titles;
