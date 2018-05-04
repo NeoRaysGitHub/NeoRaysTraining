a5e475fb59b0cbfbfde76c331d0a14adf815ab7f
Neorays-PC\Neorays <kaveri.hi@clusterindia.com>
2018-05-04 12:21:04 +0530

    printing welcome

---
+++ reactJS_Examples/helloworld/src/custom/demo1.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Demo1 extends React.Component{
+  render(){
+    return(
+      <div id="content">
+      <h1>Welcome</h1>
+      </div>
+    );
+  }
+}
+
+export default Demo1;
---
+++ reactJS_Examples/helloworld/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Demo1 from './custom/demo1';
+
+
+ReactDOM.render(<Demo1 />, document.getElementById('root'));
