ac378d3baccabe7c2f2eb861bc201abfaeaee254
Neorays-PC\Neorays <kaveri.hi@clusterindia.com>
2018-05-04 12:16:28 +0530

    getting value from input

---
+++ reactJS_Examples/forms/src/formcreation/inputBox.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class InputBox extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      data:"initial state..."
+    }
+    this.updateState=this.updateState.bind(this);
+  }
+
+  updateState(e){
+    this.setState({data:e.target.value});
+  }
+
+  render(){
+    return(
+      <div>
+      <input type="text" value={this.state.data} onChange={this.updateState}/>
+      <h4>{this.state.data}</h4>
+      </div>
+    );
+  }
+}
+export default InputBox;
---
+++ reactJS_Examples/forms/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import InputBox from './formcreation/inputBox';
+
+
+ReactDOM.render(<InputBox />, document.getElementById('root'));
